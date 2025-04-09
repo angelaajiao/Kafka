a179931b62065c3d132f3103b365b94b4c38fbbf
Huda el Ouahabi <146740689+HudaEK@users.noreply.github.com>
2025-04-09 22:07:56 +0200

    areglos de main

--- src/main.py
+++ src/main.py
@@ -1,8 +1,13 @@
-# Para lanzar el productor
-# from producer import Producer
-# Producer().run()
-
-# Para lanzar la visualización
+from threading import Thread
 from producer import Producer
-Producer().run()
+from visualizer import Visualizer
 
+if __name__ == '__main__':
+    producer_thread = Thread(target=Producer().run)
+    visualizer_thread = Thread(target=Visualizer().run)
+
+    producer_thread.start()
+    visualizer_thread.start()
+
+    producer_thread.join()
+    visualizer_thread.join()
